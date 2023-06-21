d3d512371cf1124d0778a210f20f1f982591b9c0
dinda <azzahraadinda12@gmail.com>
2023-06-21 18:38:09 +0700

    change Migrations 🧁🍷

--- database/migrations/2023_06_20_075013_create_antreans_table.php
+++ database/migrations/2023_06_20_075013_create_antreans_table.php
@@ -15,10 +15,8 @@ return new class extends Migration
             $table->increments('id_antrean');
             $table->unsignedInteger('id_pasien')->length(10);
             $table->foreign('id_pasien')->references('id_pasien')->on('pasiens');
-
             $table->unsignedInteger('id_dokter')->length(10);
             $table->foreign('id_dokter')->references('id_dokter')->on('dokters');
-            
             $table->string('status');
             $table->time('jam_temu');
             $table->timestamps();
