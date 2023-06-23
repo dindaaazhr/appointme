1a442ec4eed4463d58b31be4cd428b1a83e6eef7
Adinda Fatimah Az-Zahra <97678433+dindaaazhr@users.noreply.github.com>
2023-06-23 07:21:38 +0700

    change View FIX 🦄🫧

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -44,7 +44,6 @@
 
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/x-icon" href="images/logo-icon.png" />
-    
         <!-- bootstrap.min css -->
         <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
         <!-- Icon Font Css -->
